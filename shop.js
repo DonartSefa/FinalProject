82dce6e2eca27c0b2b819f4d6777b262e3bd8034
DonartSefa <donart.sefa13@gmail.com>
2025-01-17 22:07:43 +0100

    sad

--- shop.js
+++ shop.js
@@ -1,11 +1,16 @@
-document.querySelector('.btn').addEventListener('click', () => {
+
+const buyButtons = document.querySelectorAll('.btn');
+
+buyButtons.forEach(button => {
+  button.addEventListener('click', () => {
     const notification = document.getElementById('notification');
     
-    // Show the notification
+ 
     notification.classList.add('show');
     
-    // Hide the notification after 5 seconds
+
     setTimeout(() => {
       notification.classList.remove('show');
-    }, 5000);
+    }, 3000);
   });
+});
